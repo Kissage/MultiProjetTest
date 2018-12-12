9c61d1b9c7bb2074b44a1b115686fde8ea691a84
DUBUS <julien.dubus@promod.fr>
2018-12-12 09:55:20 +0100

    Init v0

---
+++ ModArchi-api/src/main/java/com/promod/ModArchi/App.java
@@ -0,0 +1,13 @@
+package com.promod.ModArchi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
