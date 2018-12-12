d55cdb6aa415d18137aaf77223154a0a3ab6a410
DUBUS <julien.dubus@promod.fr>
2018-12-12 14:53:39 +0100

    msg hi

--- mod-archi/src/main/java/com/promod/ModArchi/App.java
+++ mod-archi/src/main/java/com/promod/ModArchi/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! " + MesConstantes.BEAN_TOTO );
+        System.out.println( "Hi! " + MesConstantes.BEAN_TOTO );
     }
 }
